6533f12b3ed1ef69c267e88039055303afa88950
Poli <diegoroca.ordes@gmail.com>
2019-07-16 21:05:55 +0200

    Basic Movement and 3D Camera

---
+++ Assets/Scripts/ThirdPersonCamera.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class ThirdPersonCamera : MonoBehaviour
+{
+    public bool lockCursor;
+    public float mouseSensitivity = 10;
+    public Transform target;
+    public float distanceFromTarget = 2;
+    public Vector2 pitchMinMax = new Vector2(-40, 85);
+    public float rotationSmoothTime = .12f;
+
+    private Vector3 m_smoothVel;
+    private Vector3 m_currentRot;
+    private float m_xRotation;
+    private float m_yRotation;
+
+    private void Start()
+    {
+        if (target == null)
+        {
+            Debug.LogWarning("[Warning]: no target found.");
+        }
+
+        if (lockCursor)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+    }
+
+    private void LateUpdate()
+    {
+        m_xRotation += Input.GetAxis("Mouse X") * mouseSensitivity; //Se suma porque se considera que el movimiento que hagamos es positivo
+        m_yRotation -= Input.GetAxis("Mouse Y") * mouseSensitivity; //Se resta porque se considera que el movimiento que hagamos tiene que ser al reves (salvo invertir el eje)
+        m_yRotation = Mathf.Clamp(m_yRotation, pitchMinMax.x, pitchMinMax.y); //Limitamos la rotación en el eje Y
+
+        //Suavizado
+        m_currentRot = Vector3.SmoothDamp(m_currentRot, new Vector3(m_yRotation, m_xRotation), ref m_smoothVel, rotationSmoothTime);
+        transform.eulerAngles = m_currentRot;
+
+        //La posición de la camara depende de la distanceFromTarget
+        transform.position = target.position - transform.forward * distanceFromTarget;
+    }
+
+}
---
+++ Assets/Scripts/ThirdPersonCharacter.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class ThirdPersonCharacter : MonoBehaviour
+{
+    public float movementSpeed = 5f;
+
+    private Rigidbody m_RigidBody;
+    private Transform m_MainCamera;
+    private Vector3 m_CamForward;             //Direccion de movimiento
+    private Vector3 m_Move;
+
+    private void Start()
+    {
+        m_RigidBody = GetComponent<Rigidbody>();
+        m_MainCamera = Camera.main.transform;
+
+        if (m_RigidBody == null)
+        {
+            Debug.LogWarning("[Warning]: no rigidbody found.");
+        }
+        if (Camera.main == null)
+        {
+            Debug.LogWarning("[Warning]: no main camera found.");
+        }
+    }
+
+    private void Move(Vector3 movement)
+    {
+        //Si el movimiento es mayor que 1 (por ejemplo, diagonales) lo normalizamos.
+        if (movement.magnitude > 1f) movement.Normalize();
+        //Transformamos el movimiento en el mundo a un movimiento local
+        movement = transform.InverseTransformDirection(movement);
+
+        //Aplicar movimiento al rigidBody
+        m_RigidBody.MovePosition(this.transform.position + movement * Time.deltaTime * movementSpeed);
+    }
+
+
+    private void FixedUpdate()
+    {
+        //Obtener inputs
+        float xMovement = Input.GetAxis("Horizontal");
+        float yMovement = Input.GetAxis("Vertical");
+
+        //Calcular direccion movimiento respecto a la camara
+        m_CamForward = Vector3.Scale(m_MainCamera.forward, new Vector3(1, 0, 1)).normalized;
+        m_Move = yMovement * m_CamForward + xMovement * m_MainCamera.right;
+
+        //Aplicamos el movimiento
+        Move(m_Move);
+    }
+}
